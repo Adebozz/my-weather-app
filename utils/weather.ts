0e5ad35c36f6bff11a7ce3f31a7e47f2eadf9053
Adeboss <ademolaadeigbe3@gmail.com>
2024-10-24 23:58:56 +0100

    updated code

--- utils/weather.ts
+++ utils/weather.ts
@@ -1,20 +1,23 @@
-// utils/weather.ts
 import axios from 'axios';
 
-const BASE_URL = 'https://api.openweathermap.org/data/2.5';
+const BASE_URL = 'https://api.weatherapi.com/v1';
 
 export const fetchWeather = async (location: string) => {
   try {
-    const response = await axios.get(`${BASE_URL}/weather`, {
+    const response = await axios.get(`${BASE_URL}/current.json`, {
       params: {
+        key: process.env.NEXT_PUBLIC_WEATHER_API_KEY, // Adjusted for WeatherAPI
         q: location,
-        appid: process.env.NEXT_PUBLIC_WEATHER_API_KEY,
-        units: 'metric', // or 'imperial' for Fahrenheit
+        aqi: 'no', // Optional: include AQI data or not
       },
     });
     return response.data;
   } catch (error) {
     console.error('Error fetching weather data:', error);
-    throw error;
+    if (axios.isAxiosError(error) && error.response?.status === 400) {
+      throw new Error(`The location "${location}" is not valid.`);
+    } else {
+      throw new Error('An error occurred while fetching weather data. Please try again.');
+    }
   }
 };
